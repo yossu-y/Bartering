0a8ec7294851a815c33cb40a56780570a5877966
yossu-y <yangyuub@gmail.com>
2022-05-24 13:27:07 +0000

    [update]application-controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  private
+  
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+    keys: [
+    :last_name,
+    :first_name,
+    :first_name_kana,
+    :last_name_kana,
+    :post_code,
+    :address,
+    :phone_number
+    ])
+  end
+  
 end
