1b9b4416e6f3adc69cf419667492eaf207f883eb
yossu-y <yangyuub@gmail.com>
2022-05-27 09:36:53 +0000

    [add]guest-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
+  
   def show
   end
 
@@ -7,4 +10,21 @@ class UsersController < ApplicationController
 
   def edit
   end
+  
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'guestuserでログインしました。'
+  end
+  
+  private
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end  
+  
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  
   root to: "homes#top"
   get "homes/about" => "homes#about",as: "about"
   
